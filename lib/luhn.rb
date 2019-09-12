d0b54bc4c9fa2188e16412f05b884050551f1611
kathleen kalugdan <kay16_alcantara@yahoo.com.ph>
2019-09-12 17:05:43 +0000

    final commit

--- lib/luhn.rb
+++ lib/luhn.rb
@@ -2,13 +2,13 @@ module Luhn
   def self.is_valid?(number)
     number = number.to_s.chars.map(&:to_i).reverse
     number.map!.with_index do |num, i|
-      if i.odd?
+      if i % 2 !=0
          num *= 2
          num -= 9 if num >=10
         end  
      num
     end
-
+    puts number.reduce(:+)
     return true ? number.reduce(:+) % 10 == 0 : false
 end
 end
